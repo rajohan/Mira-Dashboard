82f6d376e96ebebcc7aa12925485175377877493
mira-2026 <mira-2026@agentmail.to>
2026-06-04 14:36:13 +0200

    fix(files): remove migrated cron config entry

--- src/components/features/files/index.ts
+++ src/components/features/files/index.ts
@@ -1,10 +1,5 @@
 export { ConfigSection } from "./ConfigSection";
-export {
-    CONFIG_TOP_FILES,
-    CRON_DIR_FILES,
-    HOOKS_DIR_FILES,
-    MAX_PREVIEW_SIZE,
-} from "./fileConstants";
+export { CONFIG_TOP_FILES, HOOKS_DIR_FILES, MAX_PREVIEW_SIZE } from "./fileConstants";
 export { FileContentViewer } from "./FileContentViewer";
 export { FileEditorPanel } from "./FileEditorPanel";
 export { FilesSidebar } from "./FilesSidebar";
